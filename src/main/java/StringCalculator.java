292a78049b68863d989c56a8f48c0d52833b49fa
awesomeo184 <awesomeo184@gmail.com>
2020-12-25 15:52:31 +0900

    feat: 빈값 or null을 입력 받을 경우 기능 구현

---
+++ src/main/java/StringCalculator.java
@@ -0,0 +1,10 @@
+public class StringCalculator {
+
+    public static int splitAndSum(String text) {
+        int result = 0;
+        if (text == null || text.isEmpty()) {
+            return 0;
+        }
+        return result;
+    }
+}
---
+++ src/test/java/StringCalculatorTest.java
@@ -0,0 +1,12 @@
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+public class StringCalculatorTest {
+
+    @Test
+    public void null_혹은_빈값() {
+        assertThat(StringCalculator.splitAndSum(null)).isEqualTo(0);
+        assertThat(StringCalculator.splitAndSum("")).isEqualTo(0);
+    }
+}
