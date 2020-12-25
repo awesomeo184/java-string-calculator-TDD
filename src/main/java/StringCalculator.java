eb0fe721d4663fac44df9e8898051eefa4c7efad
awesomeo184 <awesomeo184@gmail.com>
2020-12-25 17:39:35 +0900

    refactor: 메서드가 하나의 일만 하도록 분리

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -3,15 +3,32 @@ import java.util.ArrayList;
 public class StringCalculator {
 
     public static int splitAndSum(String text) {
-        int result = 0;
+        int result;
+
         if (text == null || text.isEmpty()) {
             return 0;
         }
-        String[] values = text.split(",");
-        for (String value : values) {
-            result += Integer.parseInt(value);
-        }
 
+        String[] values = text.split(",");
+        int[] ints = parseToInt(values);
+        result = sum(ints);
+
+        return result;
+    }
+
+    private static int[] parseToInt(String[] values) {
+        int[] ints = new int[values.length];
+        for (int i = 0; i < ints.length; i++) {
+            ints[i] = Integer.parseInt(values[i]);
+        }
+        return ints;
+    }
+
+    private static int sum(int[] ints) {
+        int result = 0;
+        for (int number : ints) {
+            result += number;
+        }
         return result;
     }
 }
